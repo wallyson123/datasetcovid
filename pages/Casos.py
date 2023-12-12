cf922ff700cdd65703bf4b9fc7fbdf892fc49815
Wálisson Soares <wallyson2013@hotmail.com>
2023-12-12 17:11:13 -0300

    Add files via upload

--- pages/Casos.py
+++ pages/Casos.py
@@ -10,8 +10,6 @@ except FileNotFoundError:
     st.error(f"Arquivo não encontrado: {caminho_arquivo}")
     st.stop()
 
-
-
 # Verificar se a coluna "state" está presente no DataFrame
 if "state" not in df.columns:
     st.error("A coluna 'state' não está presente no DataFrame.")
@@ -26,10 +24,8 @@ if "date" not in df.columns:
 selected_state = st.sidebar.selectbox("Selecione um estado para filtrar:", df["state"].unique())
 filtered_df = df[df["state"] == selected_state]
 
-
 # Exibir o DataFrame filtrado por estado
 st.header(f"Visualização dos Dados para o Estado {selected_state}:")
-
 st.dataframe(filtered_df)
 
 # Verificar se a coluna "date" está presente no DataFrame filtrado
@@ -49,19 +45,43 @@ monthly_incidence = filtered_df.groupby('Month').size().reset_index(name='Cases'
 # Exibir gráfico de barras para a incidência mês a mês
 st.bar_chart(monthly_incidence.set_index('Month'))
 
-# Mapa dos estados mais afetados
-st.header("Mapa dos Estados Mais Afetados:")
+# Mapa dos estados mais afetados no Brasil
+st.header("Mapa dos Estados Mais Afetados no Brasil:")
 # Agrupar por estado e contar o número total de casos
 total_cases_by_state = df.groupby('state').size().reset_index(name='Total Cases')
 
-# Criar um mapa usando Plotly Express
+# Criar um mapa usando Plotly Express para o Brasil
 fig = px.choropleth(total_cases_by_state,
                     locations='state',
-                    locationmode='USA-states',
+                    locationmode='country names',  # Use 'country names' for world maps
                     color='Total Cases',
-                    scope="usa",
-                    title='Estados Mais Afetados',
+                    title='Estados Mais Afetados no Brasil',
                     labels={'Total Cases': 'Número de Casos'}
                     )
+# Atualizar o layout do mapa para exibir o Brasil
+fig.update_geos(projection_type="natural earth")
+
+# Adicionar as informações de mortes no final do mapa
+total_deaths = df[df["state"] == selected_state][[
+    'deaths_indeterminate_2020',
+    'deaths_respiratory_failure_2020',
+    'deaths_others_2020',
+    'deaths_pneumonia_2020',
+    'deaths_septicemia_2020',
+    'deaths_sars_2020',
+    'deaths_covid19',
+    'new_deaths_indeterminate_2020',
+    'new_deaths_respiratory_failure_2020',
+    'new_deaths_others_2020',
+    'new_deaths_pneumonia_2020',
+    'new_deaths_septicemia_2020',
+    'new_deaths_sars_2020',
+    'new_deaths_total_2020'
+]].sum()
+
 # Exibir o mapa
 st.plotly_chart(fig)
+
+# Exibir informações de mortes no final do mapa
+st.subheader("Total de Mortes por Categoria:")
+st.write(total_deaths)
