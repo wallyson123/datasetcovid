83b37e0882d6e23ca5b675fe762956c3c93f149c
Wálisson Soares <wallyson2013@hotmail.com>
2023-12-12 17:35:28 -0300

    Add files via upload

--- pages/Casos.py
+++ pages/Casos.py
@@ -20,17 +20,30 @@ if "date" not in df.columns:
     st.error("A coluna 'date' não está presente no DataFrame.")
     st.stop()
 
+# Filtro por estado
+selected_state = st.sidebar.selectbox("Selecione um estado para filtrar:", df["state"].unique())
+filtered_df = df[df["state"] == selected_state]
+
+# Exibir o DataFrame filtrado por estado
+st.header(f"Visualização dos Dados para o Estado {selected_state}:")
+st.dataframe(filtered_df)
+
+# Verificar se a coluna "date" está presente no DataFrame filtrado
+if "date" not in filtered_df.columns:
+    st.error("A coluna 'date' não está presente no DataFrame filtrado.")
+    st.stop()
+
 # Filtro de casos por incidência mês a mês
 st.header("Filtro de Casos por Incidência Mês a Mês:")
 # Converter a coluna 'date' para o tipo datetime
-df['date'] = pd.to_datetime(df['date'])
+filtered_df['date'] = pd.to_datetime(filtered_df['date'])
 # Adicionar uma nova coluna 'Month' para extrair o mês
-df['Month'] = df['date'].dt.month
-# Agrupar por estado, mês e contar o número de casos
-monthly_incidence = df.groupby(['state', 'Month']).size().reset_index(name='Cases')
+filtered_df['Month'] = filtered_df['date'].dt.month
+# Agrupar por mês e contar o número de casos
+monthly_incidence = filtered_df.groupby('Month').size().reset_index(name='Cases')
 
 # Exibir gráfico de barras para a incidência mês a mês
-st.bar_chart(monthly_incidence.set_index(['state', 'Month']))
+st.bar_chart(monthly_incidence.set_index('Month'))
 
 # Mapa dos estados mais afetados no Brasil
 st.header("Mapa dos Estados Mais Afetados no Brasil:")
@@ -40,16 +53,16 @@ total_cases_by_state = df.groupby('state').size().reset_index(name='Total Cases'
 # Criar um mapa usando Plotly Express para o Brasil
 fig = px.choropleth(total_cases_by_state,
                     locations='state',
-                    locationmode='Brazil',  # Use 'Brazil' for Brazil maps
+                    locationmode='USA-states',  # Use 'USA-states' for state-level maps
                     color='Total Cases',  # Specify the column for color scale
                     color_continuous_scale='reds',  # Adjust color scale as needed
-                    title='Estados Mais Afetados no Brasil',
+                    title=f'Estados Mais Afetados no Brasil - {selected_state}',
                     labels={'Total Cases': 'Número de Casos'}
                     )
 
 # Adicionar as informações de mortes no final do mapa
 try:
-    total_deaths = df[[
+    total_deaths = filtered_df[[
         'deaths_indeterminate_2020',
         'deaths_respiratory_failure_2020',
         'deaths_others_2020',
